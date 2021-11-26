6284051c097a82eb95a97d710836e4651adcee41
h18340637145 <h18340637145@outlook.com>
2021-11-26 13:46:18 +0800

    my

---
+++ httpd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cctype>
+#include <cstring>
+#include <string>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <cstdlib>
+#include <sys/wait.h>
+
+
+using server444
